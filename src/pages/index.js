70805397294420197d35f59dd976c5a2e6505747
Christian Hein <scheiehein@gmail.com>
2020-06-12 13:00:41 +0200

    Added gatsby fundamentals

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ color: "#4a7b17" }}>
+      <h1>Velkommen til lesesalen!</h1>
+      <p>Det beste sted på jord...</p>
+    </div>
+  );
+}
