2901e91a179dd7c714617a5c4a9f1037a6bca37f
Mathias <mrboen94@gmail.com>
2020-08-25 11:46:29 +0200

    yarn upgrade and social headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-social-banners`,
+      options: {
+        // optional & defaults
+        subText: { author: "Lesesalen" },
+        imgWidth: 650,
+        imgHeight: 172,
+        x: 139,
+        y: 231,
+        outputImg: "lesesalen-header.png",
+        // required (have to be specified)
+        baseImg: path.resolve("./content/assets/lesesalen-header.png"),
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "fonts",
