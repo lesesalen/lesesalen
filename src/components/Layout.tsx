fb83a43a8553d33bc9c83e1d6f63d479d9347d8d
Anders Syvertsen <andesyv@gmail.com>
2020-10-16 15:30:30 +0200

    Spooky skeleton pop-up gifs! (#56)
    * Added a spooky message that is displayed on click
    
    * Added spooky gif loading from Giphy!
    
    Every click has a 1/5 chance of spawning a spooky skeleton gif that has been asynchronically loaded from Giphy using the "skeleton" tag at the current mouse position. The spook disappear by itself after 2 seconds, and there can only be one spook active.
    
    * Hid the API-key
    
    * Gatsby environment variable maybe

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, withPrefix } from "gatsby";
 
 import { rhythm } from "../utils/typography";
 import Nav from "./Nav";
 import StyledHeader from "./Header";
+import axios from "axios";
 
 interface Props {
   location: Record<string, string>;
   title: string;
   children: React.ReactNode;
 }
+
+interface SpookSetting {
+  x: number;
+  y: number;
+  url: string;
+};
+
+interface GiphyResponse {
+  data: { images: { original: { url: string } } };
+}
+
+// Spooky global! :o
+var spookTimer: number;
 
 const Layout: React.FC<Props> = ({ location, title, children }) => {
   const rootPath = withPrefix("/");
@@ -45,6 +59,41 @@ const Layout: React.FC<Props> = ({ location, title, children }) => {
       </h3>
     );
   }
+  
+  let [spook, setSpooks] = useState<SpookSetting | null>(null);
+
+  const renderSpook = (setting: SpookSetting) => {  
+    return (
+      <div style={{
+        position: "fixed",
+        left: setting.x,
+        top: setting.y,
+        zIndex: 10
+      }}>
+        <img src={setting.url} />
+        <div style={{
+            position: "absolute",
+            left: 10,
+            bottom: 40
+          }}>
+            Powered by Giphy
+          </div>
+      </div>
+    );
+  };
+
+  const clickEvent = async (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!Math.floor(Math.random() * 5)) {
+      event.persist();
+      const gif = await axios.get<GiphyResponse>(
+        `https://api.giphy.com/v1/gifs/random?tag=skeleton&api_key=${new String(process.env.GATSBY_GIPHY_KEY)}`,
+      );
+      setSpooks({ x: event.clientX, y: event.clientY, url: gif.data.data.images.original.url});
+      clearTimeout(spookTimer);
+      spookTimer = setTimeout(setSpooks, 2000, null);
+    }
+  };
+
   return (
     <div
       style={{
@@ -53,10 +102,11 @@ const Layout: React.FC<Props> = ({ location, title, children }) => {
         maxWidth: rhythm(24),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
+      onClick={clickEvent}
     >
       <header>{header}</header>
       <Nav />
-      <main>{children}</main>
+      <main>{spook && renderSpook(spook)}{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
