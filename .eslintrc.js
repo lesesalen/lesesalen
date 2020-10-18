15b951b654f6b4c434a0a56afed44e50a6c309fb
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-10-18 17:20:15 +0200

    Add and configure ESLint with a bunch of packages

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  plugins: ["react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier/@typescript-eslint",
+    "plugin:react/recommended",
+    "prettier/react",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  rules: {
+    // Not needed, see https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/typedef.md#when-not-to-use-it
+    "@typescript-eslint/typedef": "off",
+    "import/no-default-export": "error",
+    "import/no-unresolved": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    node: {
+      tryExtensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".node", ".mjs"],
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
