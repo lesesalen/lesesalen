4b87e8fca663d9593db9d775cbd9ca4752413b90
Mathias Bøe <mrboen94@gmail.com>
2021-03-05 19:39:08 +0100

    Feature/new components (#73)
    * added terminal component
    
    * trying to fix navigator build issue
    
    * editing build step to see if it helps
    
    * revert and retry
    
    * also window
    
    * some updates to the component
    
    * finished?
    
    * lmao bugs
    
    * Removed redundant file
    
    * Adding flashcards
    
    * Added some documentation
    
    Co-authored-by: Mathias Bøe <mathias@islandgarden.no>

---
+++ src/components/noteComponents/FlashCard.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import Show from "../../../content/assets/icons/show.svg";
+import Hide from "../../../content/assets/icons/hide.svg";
+
+const FlashCardWindow = styled.div`
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  margin-top: 12px;
+  margin-bottom: 12px;
+  width: 100%;
+  background-color: red;
+  min-height: 50px;
+  height: fit-content;
+  border-radius: 5px;
+`;
+
+const Question = styled.div`
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const Answer = styled.div<FlashCardProps>`
+  margin: 12px;
+  padding: 12px;
+  overflow-wrap: break-word;
+  color: white;
+  background: darkred;
+  border-radius: 5px;
+`;
+
+const StyledButton = styled.button`
+  padding-top: 4px;
+  border-radius: 5px;
+  border: 0px;
+  background: transparent;
+`;
+
+type FlashCardProps = {
+  question?: string;
+  children: string;
+};
+
+export default function FlashCard({
+  question,
+  children,
+}: FlashCardProps): JSX.Element {
+  const [showAnswer, setShowAnswer] = React.useState(false);
+  const onClick = () => setShowAnswer(!showAnswer);
+  return (
+    <FlashCardWindow>
+      <Question>
+        <h4
+          style={{
+            lineHeight: "185%",
+            margin: "0",
+            color: "white",
+          }}
+        >
+          {question}
+        </h4>
+        <StyledButton onClick={onClick}>
+          {showAnswer ? <Hide /> : <Show />}
+        </StyledButton>
+      </Question>
+      {showAnswer ? <Answer>{children}</Answer> : null}
+    </FlashCardWindow>
+  );
+}
